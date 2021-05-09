5e8959946d7bfc11f5f3a9ae7d5ed7fb781a6c08
Maksym Kozynets <maksym.kozynets@nure.ua>
2021-05-09 11:37:35 +0300

    Add constants class

---
+++ src/main/java/com/ua/nure/view/util/Util.java
@@ -0,0 +1,13 @@
+package com.ua.nure.view.util;
+
+public final class Util {
+    private Util() {
+    }
+
+    private static String FXML_ROOT_PATH = "/static/fxml/";
+
+    public static String SIGN_IN_PAGE_PATH = FXML_ROOT_PATH + "login.fxml";
+    public static String SIGN_UP_PAGE_PATH = FXML_ROOT_PATH + "register.fxml";
+    public static String MAIN_PAGE_PATH = FXML_ROOT_PATH + "main.fxml";
+
+}
