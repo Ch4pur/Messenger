66f2fff374aa906861abb89c7b858dd106d1838d
Maksym Kozynets <maksym.kozynets@nure.ua>
2021-05-24 20:24:25 +0300

    Added constants

---
+++ src/main/java/com/ua/nure/util/ConnectionConstants.java
@@ -0,0 +1,10 @@
+package com.ua.nure.util;
+
+public final class ConnectionConstants {
+    private ConnectionConstants() {
+    }
+
+    public static final int PORT = 3030;
+    public static final String HOST = "localhost";
+    public static final int MAX_NUMBER_OF_CLIENTS = 10;
+}
---
+++ src/main/java/com/ua/nure/util/Namings.java
@@ -0,0 +1,11 @@
+package com.ua.nure.util;
+
+public final class Namings {
+    private Namings() {}
+
+
+    public static final String SERVER_COMMANDS = "serverCommands";
+
+    public static final String LOGIN = "login";
+    public static final String PASSWORD = "password";
+}
