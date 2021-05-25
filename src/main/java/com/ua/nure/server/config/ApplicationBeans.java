6bd5048b0a9f715241df5b3ba6cf5fb6b30643fa
Maksym Kozynets <maksym.kozynets@nure.ua>
2021-05-25 19:23:47 +0300

    Put constants to server context

--- src/main/java/com/ua/nure/server/config/ApplicationBeans.java
+++ src/main/java/com/ua/nure/server/config/ApplicationBeans.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.ua.nure.util.ServerCommandNames.*;
 @Configuration
 public class ApplicationBeans {
 
@@ -23,10 +24,11 @@ public class ApplicationBeans {
     public Map<String, Command> serverCommands() {
         Map<String, Command> commands = new HashMap<>();
 
-        commands.put("signIn", context.getBean(SignInCommand.class));
-        commands.put("signUp", context.getBean(SignUpCommand.class));
-        commands.put("sendMessage", context.getBean(SendMessageCommand.class));
-        commands.put("getRoomMessages", context.getBean(GetRoomMessagesCommand.class));
+        commands.put(SIGN_IN, context.getBean(SignInCommand.class));
+        commands.put(SIGN_UP, context.getBean(SignUpCommand.class));
+        commands.put(SEND_MESSAGE, context.getBean(SendMessageCommand.class));
+        commands.put(GET_MESSAGES, context.getBean(GetRoomMessagesCommand.class));
+        commands.put(CREATE_DIALOG, context.getBean(CreateDialogRoomCommand.class));
 
         return commands;
     }
