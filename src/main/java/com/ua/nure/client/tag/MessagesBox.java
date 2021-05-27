c1d176eb6a014ba87cf10314e08cda4c748ca27b
Maksym Kozynets <maksym.kozynets@nure.ua>
2021-05-27 20:18:02 +0300

    Small change of view layer

--- src/main/java/com/ua/nure/client/tag/MessagesBox.java
+++ src/main/java/com/ua/nure/client/tag/MessagesBox.java
@@ -1,10 +1,11 @@
 package com.ua.nure.client.tag;
 
 import com.ua.nure.server.model.entity.Message;
+import javafx.scene.layout.VBox;
 
 import java.util.List;
 
-public class ChatPane {
+public class MessagesBox extends VBox {
 
     private List<Message> messages;
 }
---
+++ src/main/java/com/ua/nure/client/tag/RoomBox.java
@@ -0,0 +1,22 @@
+package com.ua.nure.client.tag;
+
+import com.ua.nure.server.model.entity.Room;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+import java.util.List;
+import java.util.Set;
+
+public class RoomBox extends AnchorPane {
+
+    private Room room;
+
+
+
+
+    private void createRoomPanes() {
+
+    }
+
+}
